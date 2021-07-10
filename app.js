5b512e3087f3b7680cc2987df91dbd3fd0d55452
lee <o5046821854@gmail.com>
2021-07-10 21:04:23 +0900

    second commit

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 //import file
 const Chalk = require('chalk')
-const Validator = require('validator')
 const importedStr = require('./notes.js')
-//const importedName = require('./utils.js')
-// const importedAdd = require('./utils.js')
 
-// const Name= 'Lee'
-
-// console.log(importedName)
-// const sum =  importedAdd(4, -2)
-// console.log (sum)
-const colorStr = Chalk.inverse.bold.red("hello world!")
-console.log(colorStr)
-console.log(importedStr())
-console.log(Validator.isEmail('dldudtls2@example.com'))
+const command = process.argv[2]
+if (command === 'add'){
+    console.log('adding note!')
+} else if(command === 'remove')
+{
+    console.log("remove note!")
+}
